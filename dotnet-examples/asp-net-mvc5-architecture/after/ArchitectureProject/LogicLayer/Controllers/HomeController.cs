b2ce693d31faaf6bc818b4b00083111a0a4b93a5
YashKorla2 <Yash.Korla2@encora.com>
2025-05-01 13:47:52 +0530

    Adding asp-net-mvc5-architecture project

---
+++ dotnet-examples/asp-net-mvc5-architecture/after/ArchitectureProject/LogicLayer/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using ServiceLayer.StudentService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LogicLayer.Controllers
+{
+    public class HomeController : BaseController
+    {
+        #region Fields
+
+        private IStudentService _StudentService;
+
+        #endregion
+
+        #region Constructor
+
+        public HomeController(IStudentService StudentService)
+        {
+            _StudentService = StudentService;
+        }
+
+        #endregion
+
+        #region Actions
+        public ActionResult Index()
+        {
+            _StudentService.AddStudent();
+            return View();
+        }
+        #endregion
+    }
+}
